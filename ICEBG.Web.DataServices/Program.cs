2b8c61fb8549007ac11e1fba43719b0ce34dfe1d
Mark Stega <mark2002@stega.us>
2022-05-16 16:26:35 -0400

    First content commit

---
+++ ICEBG.Web.DataServices/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapFallbackToPage("/index_server");
+});
+
+app.Run();
---
+++ SuperClean.cmd
@@ -0,0 +1,6 @@
+﻿@echo on
+c:
+cd \Solutions\ohi\icebg
+for /d /r . %%d in (bin,obj,ClientBin,Generated_Code,node_modules,.vs) do @if exist "%%d" rd /s /q "%%d"
+pause
+
