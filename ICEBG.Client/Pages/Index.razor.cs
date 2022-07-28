eae2feecbd1ab8b44d4ca1436995cf52f1234531
Mark Stega <mark2002@stega.us>
2022-07-28 16:47:14 -0400

    Final

--- ICEBG.Client/Pages/Index.razor.cs
+++ ICEBG.Client/Pages/Index.razor.cs
@@ -1,7 +1,8 @@
-﻿namespace ICEBG.Client.Pages
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ICEBG.Client.Pages;
+
+[Sitemap(SitemapAttribute.eChangeFreqType.Weekly, 0.8)]
+public partial class Index: ComponentBase
 {
-    [Sitemap(SitemapAttribute.eChangeFreqType.Weekly, 0.8)]
-    public partial class Index
-    {
-    }
 }
---
+++ ICEBG.Client/Pages/Weather.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
+namespace ICEBG.Client.Pages;
+
+[Sitemap(SitemapAttribute.eChangeFreqType.Weekly, 0.8)]
+public partial class Weather: ComponentBase
+{
+    private WeatherForecast[]? forecasts;
+    protected override async Task OnInitializedAsync()
+    {
+        forecasts = await ForecastService.GetForecastAsync();
+    }
+}
