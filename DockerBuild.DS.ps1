e9b7f9f72e1ff270d9efbb042f37cb032e11ea22
MarkStega <mark2002@stega.us>
2022-08-28 10:20:06 -0400

    workflow test (#8)
    * workflow test
    
    * dotnet info
    
    * dotnet --info x 2
    
    * Added default version to dockerfiles, created dockerbuild command files
    
    * Remove publish profiles
    
    * Test removal of azure login
    
    * First attempt at WIP publish using new tooling
    
    * 2nd
    
    * 3rd
    
    * 4th
    
    * 5th
    
    * 6th
    
    * 7th
    
    * 8th
    
    * 9th
    
    * 10th
    
    * 11th
    
    * 12th
    
    * 13th
    
    * 14th
    
    * 15th
    
    * 16th
    
    * 17th
    
    * 18th
    
    * 19th
    
    * 20th
    
    * 21st
    
    * 22nd
    
    * 23rd
    
    * 24th
    
    * 25th
    
    * 26th
    
    * 27th
    
    * 28th

---
+++ DockerBuild.DS.ps1
@@ -0,0 +1,1 @@
+docker build --file ./ICEBG.Web.UserInterface/Dockerfile . --progress=plain
--- ICEBG.Client/Data/WeatherForecastService.cs
+++ ICEBG.Client/Data/WeatherForecastService.cs
@@ -13,9 +13,7 @@ namespace ICEBG.Client
         {
             using (var client = new System.Net.Http.HttpClient())
             {
-                // Call *mywebapi*, and display its response in the page
                 var request = new System.Net.Http.HttpRequestMessage();
-                // webapi is the container name
                 request.RequestUri = new Uri(ApplicationConfiguration.pWeatherEndpoint);
                 var response = await client.SendAsync(request);
                 var forecasts = await response.Content.ReadFromJsonAsync<WeatherForecast[]>();
