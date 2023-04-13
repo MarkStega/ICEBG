945b569c4157aae144be038f6e3244521f674638
Mark Stega <mark2002@stega.us>
2023-04-13 16:02:56 -0400

    DB & DN8P3

--- SuperClean.cmd
+++ SuperClean.cmd
@@ -1,6 +1,6 @@
 @echo on
 c:
 cd \Solutions\ohi\icebg
-for /d /r . %%d in (bin,obj,ClientBin,Generated_Code,node_modules,.vs) do @if exist "%%d" rd /s /q "%%d"
+for /d /r . %%d in (bin,obj,node_modules,.vs) do @if exist "%%d" rd /s /q "%%d"
 pause
 
