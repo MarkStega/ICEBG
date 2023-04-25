c45127388709817a960b1db3e6c486e3c02ee9ad
Mark Stega <mark2002@stega.us>
2023-04-25 13:34:25 -0400

    Add notification service

--- ICEBG.Blazor/Program.cs
+++ ICEBG.Blazor/Program.cs
@@ -19,6 +19,16 @@ using ICEBG.Client.Infrastructure.ClientServices;
 using ICEBG.AppConfig;
 using ICEBG.Client;
 using Microsoft.AspNetCore.Components.Web;
+using Blazored.LocalStorage;
+using GoogleAnalytics.Blazor;
+using Material.Blazor;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.CookiePolicy;
+using Serilog;
+using Serilog.Events;
+using Serilog.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace ICEBG.Blazor;
 public class Program
@@ -36,6 +46,8 @@ public class Program
         builder.Services.AddScoped(
             sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+        builder.Services.AddScoped<INotification, WebAssemblyNotificationService>();
+
         builder.Services.AddLogging(builder => builder
             .AddBrowserConsole() // Add Blazor.Extensions.Logging.BrowserConsoleLogger
             .SetMinimumLevel(LogLevel.Information)
@@ -44,6 +56,41 @@ public class Program
         builder.Services.AddOptions();
 
         ClientServices.Inject(ApplicationConfiguration.pDataServicesEndpointPrefix, builder.Services);
+
+        builder.Services.Configure<CookiePolicyOptions>(options =>
+        {
+            // Has Pentest fixes
+            options.CheckConsentNeeded = context => true;
+            options.HttpOnly = HttpOnlyPolicy.Always;
+            options.MinimumSameSitePolicy = SameSiteMode.Strict;
+            options.Secure = CookieSecurePolicy.Always;
+        });
+
+        builder.Services.AddBlazoredLocalStorage();
+
+        Log.Logger = new LoggerConfiguration()
+#if DEBUG
+.MinimumLevel.Debug()
+#else
+            .MinimumLevel.Information()
+#endif
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("GoogleAnalytics.Blazor", LogEventLevel.Debug)
+            .Enrich.FromLogContext()
+            .WriteTo.Async(a => a.BrowserConsole(outputTemplate: "{Timestamp:HH:mm:ss.fff}\t[{Level:u3}]\t{Message}{NewLine}{Exception}"))
+            .CreateLogger();
+
+        builder.Logging.AddProvider(new SerilogLoggerProvider());
+
+        builder.Services.AddGBService(options =>
+        {
+            options.TrackingId = "G-2VZJ2X14RH";
+            options.GlobalEventParams = new Dictionary<string, object>()
+    {
+        { Utilities.EventCategory, Utilities.DialogActions },
+        { Utilities.NonInteraction, true },
+    };
+        });
 
         await builder.Build().RunAsync();
     }
