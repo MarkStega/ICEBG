d721c737696c7deaf7ed4fa1fb650efa030af9ac
Mark Stega <mark2002@stega.us>
2023-04-30 14:57:27 -0400

    Add carousel

--- ICEBG.Client/Pages/Index.razor.cs
+++ ICEBG.Client/Pages/Index.razor.cs
@@ -5,4 +5,29 @@ namespace ICEBG.Client.Pages;
 [Sitemap(SitemapAttribute.eChangeFreqType.Weekly, 0.8)]
 public partial class Index: ComponentBase
 {
+
+    private class ImageData
+    {
+        public string Uri { get; set; } = "";
+        public string Caption { get; set; } = "";
+        public string Width { get; set; } = "";
+        public string Height { get; set; } = "";
+        public bool Preload { get; set; } = true;
+        public string Rel => Preload ? "preload" : "prefetch";
+    }
+
+    private static readonly ImageData[] CarouselImages = new ImageData[]
+    {
+        new() { Uri = "_content/ICEBG.Client/images/01-main-screen.webp", Caption = "Dioptra's main screen layout", Preload = true },
+        new() { Uri = "_content/ICEBG.Client/images/02-main-screen-search.webp", Caption = "Scheme search" },
+        new() { Uri = "_content/ICEBG.Client/images/03-march-costs-chart.webp", Caption = "Budget, actual and forecast development costs" },
+        new() { Uri = "_content/ICEBG.Client/images/04-march-accruals.webp", Caption = "Loan interest accrual details" },
+        new() { Uri = "_content/ICEBG.Client/images/05-march-cost-summary.webp", Caption = "Project cost summary" },
+        new() { Uri = "_content/ICEBG.Client/images/06-march-capital-flows.webp", Caption = "Actual/forecast capital structure" },
+        new() { Uri = "_content/ICEBG.Client/images/07-march-land-reg.webp", Caption = "UK Land Registry sold unit prices" },
+        new() { Uri = "_content/ICEBG.Client/images/08-march-version-graph.webp", Caption = "Scheme data versioning/audit" },
+        new() { Uri = "_content/ICEBG.Client/images/09-march-edit-budget-schedule.webp", Caption = "Editting cost budget schedules" },
+    };
+
+
 }
