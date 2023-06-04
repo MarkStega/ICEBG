739b3fbb33e698b3a3915b754ca9afd2bba2014f
Mark Stega <mark2002@stega.us>
2023-06-04 09:13:29 -0400

    Migrate 13.5 resources to 13.0

---
+++ ICEBG.Client/Components/FlowLabeledText.razor.cs
@@ -0,0 +1,16 @@
+﻿using ICEBG.AppConfig;
+using ICEBG.DataTier.DataDefinitions;
+using ICEBG.DataTier.HelperClasses;
+
+using Microsoft.AspNetCore.Components;
+
+namespace ICEBG.Client.Components;
+
+public partial class FlowLabeledText
+{
+    [Parameter] public string count { get; set; }
+    [Parameter] public string count2 { get; set; }
+    [Parameter] public string time { get; set; }
+    [Parameter] public ServiceResult<Configuration_DD> configuration { get; set; }
+}
+
