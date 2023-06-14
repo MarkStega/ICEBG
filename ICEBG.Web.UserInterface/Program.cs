00d19785d7b2a662f6196bc98b8fdf2135a388af
Mark Stega <mark2002@stega.us>
2023-06-14 13:04:14 -0400

    P5 changes

--- ICEBG.Web.UserInterface/Program.cs
+++ ICEBG.Web.UserInterface/Program.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,25 +29,25 @@ using NLog;
 using NLog.Web;
 
 // NLog: setup the logger first to catch all errors
-//Logger logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+Logger logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
 try
 {
-    //logger.Debug("______________________________________________________________________");
-    //logger.Debug("Building and Starting Host in Main() for ICEBG.Web.UserInterface");
+    logger.Debug("______________________________________________________________________");
+    logger.Debug("Building and Starting Host in Main() for ICEBG.Web.UserInterface");
     var builder = WebApplication.CreateBuilder(args);
 
     builder.Logging.ClearProviders();
     builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
     //builder.Host.UseNLog();
 
-    //logger.Debug("");
+    logger.Debug("");
 
-    //logger.Debug("ApplicationConfiguration.Initialize");
+    logger.Debug("ApplicationConfiguration.Initialize");
     ApplicationConfigurationServer.Initialize(builder);
 
     // Add services to the container.
-    //logger.Debug("ClientServices.Inject");
+    logger.Debug("ClientServices.Inject");
     ClientServices.Inject(ApplicationConfiguration.pDataServicesEndpointPrefix, builder.Services);
 
     //  Response compression
@@ -149,17 +150,17 @@ try
 
     builder.Services.AddResponseCaching();
 
-    //logger.Debug("Adding razor pages");
+    logger.Debug("Adding razor pages");
     builder.Services.AddRazorPages();
 
     // Needed for prerendering on WebAssembly as well as general use
     builder.Services.AddTransient<INotification, ServerNotificationService>();
 
 #if BLAZOR_SERVER
-    //logger.Debug("AddMvc");
+    logger.Debug("AddMvc");
     builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 
-    //logger.Debug("Add server side blazor");
+    logger.Debug("Add server side blazor");
     builder.Services.AddServerSideBlazor();
 #endif
 
@@ -170,7 +171,7 @@ try
         options.MaxAge = TimeSpan.FromDays(365);
     });
 
-    //logger.Debug("Configure<CookiePolicyOptions>");
+    logger.Debug("Configure<CookiePolicyOptions>");
     builder.Services.Configure<CookiePolicyOptions>(options =>
     {
         options.CheckConsentNeeded = context => true;
@@ -179,19 +180,19 @@ try
         options.Secure = CookieSecurePolicy.Always;
     });
 
-    //logger.Debug("AddOptions");
+    logger.Debug("AddOptions");
     builder.Services.AddOptions();
 
-    //logger.Debug("AddMemoryCache");
+    logger.Debug("AddMemoryCache");
     builder.Services.AddMemoryCache();
 
-    //logger.Debug("AddHttpContextAccessor");
+    logger.Debug("AddHttpContextAccessor");
     builder.Services.AddHttpContextAccessor();
 
-    //logger.Debug("AddBlazoredLocalStorage");
+    logger.Debug("AddBlazoredLocalStorage");
     builder.Services.AddBlazoredLocalStorage();
 
-    //logger.Debug("AddGBService");
+    logger.Debug("AddGBService");
     builder.Services.AddGBService(options =>
     {
         options.TrackingId = "G-2VZJ2X14RH";
@@ -204,13 +205,23 @@ try
 
     builder.Services.AddCompressedStaticFiles();
 
-    //logger.Debug("Add Grpc");
+    logger.Debug("Add Grpc");
     builder.Services.AddGrpc(options =>
     {
         options.EnableDetailedErrors = true;
         options.MaxReceiveMessageSize = null;
         options.MaxSendMessageSize = null;
     });
+
+    builder.Services.AddRateLimiter(_ => _
+        .AddFixedWindowLimiter(policyName: "fixed", options =>
+        {
+            options.PermitLimit = 1;
+            options.Window = TimeSpan.FromSeconds(1);
+            options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+            options.QueueLimit = 10;
+        }
+        ));
 
     var app = builder.Build();
 
@@ -249,21 +260,7 @@ try
     app.UseRouting();
 
     // Limit api calls to 10 in a second to prevent external denial of service.
-    app.UseRateLimiter(new()
-    {
-        GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
-        {
-            return RateLimitPartition.GetFixedWindowLimiter("GeneralLimit",
-                _ => new FixedWindowRateLimiterOptions()
-                {
-                    Window = TimeSpan.FromSeconds(1),
-                    PermitLimit = 1,
-                    QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
-                    QueueLimit = 10,
-                });
-        }),
-        RejectionStatusCode = 429,
-    });
+    app.UseRateLimiter();
 
     app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
 
@@ -273,17 +270,17 @@ try
 
     app.MapGet("/sitemap.xml", async context => { await Sitemap.Generate(context); });
 
-    //logger.Debug("Completing startup, executing app.Run()...");
-    //logger.Debug(" ");
+    logger.Debug("Completing startup, executing app.Run()...");
+    logger.Debug(" ");
     await app.RunAsync();
 }
 catch (Exception ex)
 {
-    //logger.Fatal(ex, "Unhandled exception");
+    logger.Fatal(ex, "Unhandled exception");
 }
 finally
 {
     // Ensure message flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-    //logger.Debug("Shutting down NLOG");
+    logger.Debug("Shutting down NLOG");
     LogManager.Shutdown();
 }
