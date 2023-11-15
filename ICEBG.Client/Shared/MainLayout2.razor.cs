3c5b6563132da2a393e5b984719089df48907eb8
Mark Stega <mark2002@stega.us>
2023-11-15 16:44:09 -0500

    2023-11-15 EoD

---
+++ ICEBG.Client/Shared/MainLayout2.razor.cs
@@ -0,0 +1,104 @@
+﻿using System.Threading.Tasks;
+
+using GoogleAnalytics.Blazor;
+
+using Material.Blazor;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ICEBG.Client;
+
+/// <summary>
+/// The standard Blazor MainLayout component.
+/// </summary>
+public partial class MainLayout2 : LayoutComponentBase
+{
+    [Inject] private INotification Notifier { get; set; } = default!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+    [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+    [Inject] private IGBAnalyticsManager AnalyticsManager { get; set; } = default!;
+
+
+    private MBMenuItem[] menuItems = new MBMenuItem[]
+    {
+            new MBMenuItem {
+                Headline="One",
+                HeadlineColor="darkblue",
+                MenuItemType=MBMenuItemType.Regular },
+            new MBMenuItem {
+                Headline="Two",
+                HeadlineColor="darkblue",
+                MenuItemType=MBMenuItemType.Regular },
+            new MBMenuItem {
+                Headline="Three",
+                HeadlineColor="darkblue",
+                MenuItemType=MBMenuItemType.Regular },
+            new MBMenuItem
+            {
+                MenuItemType=MBMenuItemType.Divider
+            },
+            new MBMenuItem {
+                Headline="Four",
+                HeadlineColor="darkblue",
+                MenuItemType=MBMenuItemType.Regular,
+                LeadingIcon=MBIcon.IconDescriptorConstructor(
+                                    name: "home",
+                                    color: "darkblue")},
+            new MBMenuItem {
+                Headline="Five",
+                HeadlineColor="darkblue",
+                MenuItemType=MBMenuItemType.Regular,
+                TrailingIcon=MBIcon.IconDescriptorConstructor(
+                                    name: "alarm",
+                                    color: "darkblue")},
+            new MBMenuItem {
+                Headline="Six",
+                HeadlineColor="darkblue",
+                MenuItemType=MBMenuItemType.Regular,
+                LeadingIcon=MBIcon.IconDescriptorConstructor(
+                                    name: "home",
+                                    color: "darkblue"),
+                TrailingIcon=MBIcon.IconDescriptorConstructor(
+                                    name: "alarm",
+                                    color: "darkblue")},
+            new MBMenuItem
+            {
+                MenuItemType=MBMenuItemType.Divider
+            },
+            new MBMenuItem {
+                Headline="Seven",
+                HeadlineColor="darkblue",
+                MenuItemType=MBMenuItemType.Regular,
+                LeadingIcon=MBIcon.IconDescriptorConstructor(
+                                    name: "done",
+                                    color: "darkgreen"),
+                SuppressLeadingIcon=true },
+            new MBMenuItem {
+                Headline="Eight",
+                HeadlineColor="darkblue",
+                MenuItemType=MBMenuItemType.Regular,
+                LeadingIcon=MBIcon.IconDescriptorConstructor(
+                                    name: "done",
+                                    color: "darkgreen"),
+                SuppressLeadingIcon=true },
+            new MBMenuItem
+            {
+                MenuItemType=MBMenuItemType.Divider
+            },
+            new MBMenuItem {
+                Headline="Nine (disabled)",
+                HeadlineColor="darkblue",
+                IsDisabled=true,
+                MenuItemType=MBMenuItemType.Regular },
+    };
+
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("ICEBG.General.instantiateErrorDialog");
+        }
+    }
+}
