231ab3542e67a20bedb9886b8c175078d8e330fc
Mark Stega <mark2002@stega.us>
2024-06-12 14:08:43 -0400

    2024-06-12 Move consume resources to the index page.

--- ICEBG.Client/Pages/Index.razor.cs
+++ ICEBG.Client/Pages/Index.razor.cs
@@ -1,8 +1,12 @@
-﻿using System.Threading.Tasks;
-
-using Materia.Blazor;
+﻿using ICEBG.AppConfig;
+using ICEBG.DataTier.DataDefinitions;
+using ICEBG.DataTier.HelperClasses;
 
 using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Threading.Tasks;
+using System.Timers;
 
 namespace ICEBG.Client;
 
@@ -43,6 +47,72 @@ public partial class Index : ComponentBase
         new() { Uri = "_content/ICEBG.Client/images/programmer-420.webp", Caption = "Programmer working at a desk", Width = "420px", Height = "420px" },
         new() { Uri = "_content/ICEBG.Client/images/programmer-320.webp", Caption = "Programmer working at a desk", Width = "320px", Height = "420px" },
     };
+
+    private System.Timers.Timer pTimer { get; set; }
+
+    private string count1 { get; set; } = "'Not yet initialized'";
+    private string count2 { get; set; } = "0";
+    private string duration { get; set; } = "'Not yet initialized'";
+    private DateTime startTime { get; set; } = DateTime.MinValue;
+    private string time { get; set; } = "'Not yet initialized'";
+    private ServiceResult<Configuration_DD> configuration { get; set; }
+
+    private int internalCount = 0;
+    private int internalCount2 = 0;
+
+    #region LoadReportCollectionAsync
+    private async Task LoadReportCollectionAsync()
+    {
+        configuration = await ConfigurationClient.SelectAsync(ApplicationConfiguration.pConfigurationIdentifier);
+        internalCount += 1;
+        count1 = internalCount.ToString("N0");
+
+        var currentTime = DateTime.Now;
+        if (startTime == DateTime.MinValue)
+        {
+            startTime = currentTime;
+        }
+
+        duration = (currentTime - startTime).ToString(@"d\.hh\:mm\:ss");
+
+        time = currentTime.ToString();
+        if (internalCount >= 1000000)
+        {
+            internalCount = 0;
+            internalCount2 += 1;
+            count2 = internalCount2.ToString("N0");
+        }
+        StateHasChanged();
+        pTimer?.Dispose();
+        pTimer = new System.Timers.Timer(10);
+        pTimer.Elapsed += RefreshTimerTick;
+        pTimer.Enabled = true;
+    }
+
+    #endregion
+
+    protected override void OnAfterRender(bool isFirstRender)
+    {
+        if (isFirstRender)
+        {
+            RefreshTimerTick(null, null);
+        }
+    }
+
+    #region RefreshTimerTick
+    public void RefreshTimerTick(Object source, ElapsedEventArgs e)
+    {
+        InvokeAsync(LoadReportCollectionAsync);
+    }
+    #endregion
+
+    #region IDispose
+    void IDisposable.Dispose()
+    {
+        pTimer?.Dispose();
+    }
+
+    #endregion
 
 
 }
